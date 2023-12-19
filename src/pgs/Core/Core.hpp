70d40f7c5facc0d0f33a55fee69a826d0d6e739d
Jeekun Park <jeekpark@cx1r1s2.42seoul.kr>
2023-12-19 17:50:23 +0900

    feat: sun

--- src/pgs/Core/Core.hpp
+++ src/pgs/Core/Core.hpp
@@ -11,6 +11,11 @@
 
 #pragma once
 
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/Color.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Event.hpp"
+#include "SFML/Window/VideoMode.hpp"
 #include "pgs/common.hpp"
 
 
@@ -19,7 +24,41 @@ namespace Pgs
 	class Core
 	{
 	public:
-		void run();
+		void run()
+		{
+			mScale =  0.000021541139268;
+			mSunRadius = 1392684. / 2;
+			mWindow.create(sf::VideoMode(WINDOW_X, WINDOW_Y), "a");
+			mWindow.setFramerateLimit(60);
+
+			mSun.setFillColor(sf::Color::Red);
+			mSun.setPosition((float)WINDOW_X / 2, (float)WINDOW_Y / 2);
+			mSun.setRadius(50.f);
+			mSun.setOrigin(mSun.getRadius(), mSun.getRadius());
+			while (mWindow.isOpen())
+			{
+				sf::Event event;
+				while (mWindow.pollEvent(event))
+				{
+					if (event.type == sf::Event::Closed)
+					{
+						mWindow.close();
+					}
+					
+				}
+
+				mWindow.clear();
+
+				mWindow.draw(mSun);
+				mWindow.display();
+			}
+		}
 	private:
+		double mScale;
+		double mSunRadius;//km
+		sf::RenderWindow mWindow;
+		sf::CircleShape mSun;
+
+		
 	};
 }
